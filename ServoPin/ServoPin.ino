fb6a2463e89cef731be919a1ad1c39d09367b060
SONNY <95080413+SONNYisCoding@users.noreply.github.com>
2021-11-26 19:36:22 +0700

    Add files via upload

---
+++ ServoPin/ServoPin.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>// open servo control library
+#define servoPin 11
+Servo myServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(servoPin);
+  myServo.write(0);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*for(int i = 0; i<4; i++){
+    if(i = 1){
+      myServo.write(180);
+      delay(2000);
+      myServo.write(0);
+      delay(2000);
+      myServo.write(180);
+      delay(2000);
+      myServo.write(0);
+      delay(2000);
+      Serial.println('1');
+    }*/
+      myServo.write(180);
+      delay(2000);
+      myServo.write(0);
+      delay(2000);
+      myServo.write(180);
+      delay(2000);
+      myServo.write(0);
+      delay(2000);
+      Serial.println('2');
+    
+    /*if(i = 3){
+      myServo.write(45);
+      delay(2000);
+      myServo.write(45+45);
+      delay(2000);
+      myServo.write(0);
+      delay(2000);
+      Serial.println('3');
+    }*/
+  }
+
